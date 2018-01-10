cf408c86a68e3682cf3fed856f5ce0e305bc09a2
AlejandroVC67 <vcalejandro97@gmail.com>
2018-01-09 19:11:19 -0500

    Deleted public folder

--- public/gallery/main.js
+++
@@ -1,7 +0,0 @@
-
-import { Gallery } from './gallery.js'
-import data from './gallerySource.js'
-
-/* eslint-disable */
-new Gallery(document.querySelector('.gallery-container'), data)
-/* eslint-enable */
--- public/main.js
+++
@@ -1,7 +0,0 @@
-
-import { Gallery } from './gallery/gallery.js'
-import data from './gallery/gallerySource.js'
-
-/* eslint-disable */
-new Gallery(document.querySelector('.gallery-container'), data)
-/* eslint-enable */
