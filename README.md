f8157a056e6eefca949835016eae455d7fbd44ac
AlejandroVC67 <vcalejandro97@gmail.com>
2018-01-19 13:59:22 -0500

    now using ajax

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # hb-week4-js-dom
+To run this exercise use npm run start:windows
+If you're using the seriesSource.js file you don't need the const value on the main.js called DataJSON
+The JSON Format used on the page that gives the data is the next:
+[
+  {
+    "title": "Vikings",
+    "url": "http://www.entertainmentwallpaper.com/images/res1920x1200/movie/tv-vikings08.jpg",
+    "category": "History",
+    "year": "2013",
+    "description": "Vikins is inspired by the sagas of Viking Ragnar Lothbrok one of the best-known legendary norse heroes"
+  }
+]
--- src/series/main.js
+++ src/series/main.js
@@ -1,19 +1,23 @@
 import seriesData from './seriesSource.js'
 import { Categories } from './categories.js'
 import { Grid } from './grid.js'
+import DataRetriever from './DataRetriever.js'
 
- const categories = []
- seriesData.map(element => {
-   if (!categories.includes(element.category)) {
-     categories.push(element.category)
-   }
- })
+const categories = []
+seriesData.map(element => {
+  if (!categories.includes(element.category)) {
+    categories.push(element.category)
+  }
+})
 
-function onChange (currentCategory) {
-  grid.updateGrid(currentCategory)
-}
+const URL = 'https://api.myjson.com/bins/8g3o5'
+DataRetriever.get(URL, (data) => {
+  const grid = new Grid(document.querySelector('.series'), data)
+  function onChange (currentCategory) {
+    grid.updateGrid(currentCategory)
+  }
+  const filter = new Categories(document.querySelector('.filter__list'), categories, onChange)
+})
 
 /* eslint-disable */
-const filter = new Categories(document.querySelector('.filter__list'), categories, onChange)
-const grid = new Grid(document.querySelector('.series'), seriesData)
 /* eslint-enable */
